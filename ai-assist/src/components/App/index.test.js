a702af59a7a406bf641955b91b508459dc628e41
Rasmi R U <rasmiravindranu@gmail.com>
2023-08-12 13:50:50 +1000

    Remove unused packages.

--- ai-assist/src/components/App/index.test.js
+++ ai-assist/src/components/App/index.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './index';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const title = screen.getByText(/AI-Assist/i);
+  expect(title).toBeInTheDocument();
 });
