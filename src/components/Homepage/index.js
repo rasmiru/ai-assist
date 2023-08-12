0405581d4a354d108aade0220a0e4bfe9fd5f592
Rasmi R U <rasmiravindranu@gmail.com>
2023-08-12 16:24:13 +1000

    Add hero component

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -1,10 +1,17 @@
 import Navbar from "../NavBar"
+import Hero from "../Hero";
 import Footer from "../Footer";
 
 const Homepage = () => {
     return (
         <>
             <Navbar />
+            <Hero heading={"AI Assist"} text={
+                `AI Assist lets you chat with different AI characters. 
+                They all have their own names, styles, and skills. 
+                They can help you with all kinds of things, like health, learning, cooking, finance, or fitness.            
+                `
+            } />
             <Footer />
         </>
     );
